364db9a0f8f579f19e37a29aaaeb56af5872582c
Maria Eugenia Chacon <jenimech2@gmail.com>
2011-11-23 14:40:23 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContactRails31::Application.config.secret_token = 'd264d459d36359ada216a1234d2240f0d1aca6bb1bb699c4e88cdb99f75b050f25d0eddccaa658da16fba43c91602a24a8ab6a7ed2d01dce84e6f72d66f8fc10'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ContactRails31::Application.config.session_store :cookie_store, key: '_contact_rails_3_1_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ContactRails31::Application.config.session_store :active_record_store
